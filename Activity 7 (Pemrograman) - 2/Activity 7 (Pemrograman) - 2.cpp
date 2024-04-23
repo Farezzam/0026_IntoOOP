411f53ce2199eed6cda0146b9da56750e8bb2e2c
ASUS <faris13abiyu@gmail.com>
2024-04-23 18:54:29 +0700

    isi public 'class bangundatar'

--- Activity 7 (Pemrograman) - 2/Activity 7 (Pemrograman) - 2.cpp
+++ Activity 7 (Pemrograman) - 2/Activity 7 (Pemrograman) - 2.cpp
@@ -5,4 +5,23 @@ class bangunDatar {
 
 private:
 	float panjang, lebar;
+public:
+	float luas;
+
+	void input() {
+		cout << "Masukan Panjangnya = ";
+		cin >> panjang;
+		cout << "Masukan Lebarnya = ";
+		cin >> lebar;
+	}
+
+	float hitungLuas() {
+		return panjang * lebar;
+	}
+
+	void display() {
+		cout << "Panjangnya = " << panjang << endl;
+		cout << "Lebarnya = " << lebar << endl;
+		cout << "Luasnya = " << hitungLuas() << endl;
+	}
 };

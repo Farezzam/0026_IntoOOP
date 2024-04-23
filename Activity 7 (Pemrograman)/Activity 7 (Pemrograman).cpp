62996d70b0ac736794bd9cceca8ea21900ba8e2c
ASUS <faris13abiyu@gmail.com>
2024-04-23 18:36:39 +0700

    Public 'class mahasiswa'

--- Activity 7 (Pemrograman)/Activity 7 (Pemrograman).cpp
+++ Activity 7 (Pemrograman)/Activity 7 (Pemrograman).cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 using namespace std;
 
-class Mahasiswa {
+class Mahasiswa{
+public: 
+	string nama;
+	int umur;
+	string jurusan;
 
+	void output() {
+		cout << "Nama: " << nama << endl;
+		cout << "Umur: " << umur << endl;
+		cout << "Jurusan: " << jurusan << endl;
+	}
 };
